3b2b2e1fd24e407aa918cb5e15aa39fc279db950
Hima Bindu Samudrala <hima.samudrala@techgrit.com>
2024-03-25 11:25:49 +0530

    Inital Commit

--- src/main/java/com/ecom/repository/ProductRepository.java
+++ src/main/java/com/ecom/repository/ProductRepository.java
@@ -2,6 +2,8 @@ package com.ecom.repository;
 
 import com.ecom.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
 }
